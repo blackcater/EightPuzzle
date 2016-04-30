bbdc9a4fd5cf7069fd8f2a6ba85848aead8218c6
夜猫子丶 <164710166@qq.com>
2016-04-30 12:27:01 +0800

    算法实现完成,正在检验

---
+++ src/components/AnimateStageComp/AnimateStageComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AnimateStageComp extends Component {
+  render(){
+    return (
+      <div class="animateStage">
+        AnimateStage Component
+      </div>
+    )
+  }
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,23 +5,25 @@ import React from 'react';
 import InitialStageComp from './InitialStageComp/InitialStageComp';
 import AnimateStageComp from './AnimateStageComp/AnimateStageComp';
 
-import {shuffleArr, getInversionNumber} from './Functions';
+import {shuffleArr, getInversionNumber, getExchangeProcess} from './Functions';
 
 import styles from './Main.css';
 
 let EightPuzzle = [0, 1, 2, 3, 4, 5, 6, 7, 8]; // 0表示空白方格
+let arr1 = shuffleArr(EightPuzzle);
+let arr2 = shuffleArr(EightPuzzle);
+// 如果他们的逆序数不相等, 在打乱,直到相等
+while (getInversionNumber(arr1, true) !== getInversionNumber(arr2, true)) {
+  arr2 = shuffleArr(EightPuzzle);
+}
+
+let tiktok = getExchangeProcess(arr1, arr2);
 
 class AppComponent extends React.Component {
   clickHandler(){
-    this.refs.container.children = <AnimateStageComp />;
+    this.refs.container.children = <AnimateStageComp tiktok={tiktok} startArr={arr1} endArr={arr2} />;
   }
   render() {
-    var arr1 = shuffleArr(EightPuzzle);
-    var arr2 = shuffleArr(EightPuzzle);
-    // 如果他们的逆序数不相等, 在打乱,直到相等
-    while (getInversionNumber(arr1, true) !== getInversionNumber(arr2, true)) {
-      arr2 = shuffleArr(EightPuzzle);
-    }
     return (
       <div className={styles.stage}>
         <div className={styles.container} ref="container">
